8b658a4a328c20b0ab4e35fd8699f932bb589be9
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-28 00:44:40 +1200

    🦄 refactor(refax: Move server and http modules to separate files  This commit refactors the code by moving the `Server` and `http` modules to separate files. This improves code organization and maintainability.):

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on port {}", self.addr)
-    }
-}
