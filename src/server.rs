59143723e20fa8015b71c3ba885fc9090fbd2348
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-28 23:51:05 +1200

    Add TCP listener and request handling to server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
     addr: String,
@@ -11,6 +13,31 @@ impl Server {
     }
 
     pub fn run(self) {
-        println!("Listening on port {}", self.addr)
+        println!("Listening on port {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e)
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+
+            let res = listener.accept();
+
+            if res.is_err() {
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
+        }
     }
 }
