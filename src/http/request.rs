126af8d1a6e265aecc18dd92634618cab997fc9b
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-30 07:25:18 +1200

    Implement TryFrom trait for Request
    This commit implements the TryFrom trait for the Request struct in the http module. This allows for converting a byte array into a Request object, handling parsing errors with the ParseError type.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,61 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Result as FmtResult, Formatter};
+use std::str;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method
 }
+
+// impl Request {
+//     fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
+// }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        match str::from_utf8(buf) {
+            Ok(request) => {},
+            Err(_) => return Err(ParseError::InvalidEncoding)
+        }
+        unimplemented!()
+    }
+}
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method"
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+
+impl Error for ParseError {}
