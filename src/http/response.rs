a37f3250c83f431247ba672a60efbbb26e48f748
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-01 23:03:17 +1200

    Update Response struct to use generic Write trait for sending HTTP responses

--- src/http/response.rs
+++ src/http/response.rs
@@ -3,7 +3,6 @@ use std::io::{Write, Result as IoResult};
 use std::fmt::{Display, Formatter, Result as FmtResult};
 use super::StatusCode;
 
-
 #[derive(Debug)]
 pub struct Response {
     status_code: StatusCode,
@@ -16,7 +15,7 @@ impl Response {
         Response { status_code, body }
     }
 
-    pub fn send(&self, stream:  &mut TcpStream) -> IoResult<()> {
+    pub fn send(&self, stream:  &mut impl Write) -> IoResult<()> {
         let body = match &self.body {
             Some(b) => b,
             None => "",
