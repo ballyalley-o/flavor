f130a16de27421d6ecddf58b7807441f822552ba
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-01 21:54:57 +1200

    refactor: Update Request struct to use QueryString module for parsing query strings

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
-use crate::http::Request;
+use crate::http::response;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::convert::TryInto;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -28,11 +29,19 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(StatusCode::Ok, Some("<h1>ITS WORKING!</h1>".to_string()))
                                 },
-                                Err(e) => println!("Failed to parse a request: {}", e)
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                             let _res: &Result<Request, _> = &buffer[..].try_into();
                         },
